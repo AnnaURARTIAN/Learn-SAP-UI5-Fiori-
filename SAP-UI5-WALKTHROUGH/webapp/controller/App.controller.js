476ac1e9425d94f5673a616e477882a5e7658a62
Anna Sargsyan <anna.sargsyan@urartian.am>
2022-09-06 07:19:22 +0000

    feat: add MessageToast

--- SAP-UI5-WALKTHROUGH/webapp/controller/App.controller.js
+++ SAP-UI5-WALKTHROUGH/webapp/controller/App.controller.js
@@ -1,11 +1,11 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast"
-], function(Controller) {
+], function(Controller, MessageToast) {
     'use strict';
    return Controller.extend("sap.ui.controller.wolkthrough.App",{
     onShowHello: function(){
-        alert("Hello there!")
+        MessageToast.show("Hello there!")
     }
    }) 
 });
